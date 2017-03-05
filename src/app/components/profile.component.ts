f173c88476888ee6597f7a11b788932db7ac1d88
Meng Jia <mengjia@MacBook-Pro-de-Meng.local>
2017-03-04 21:10:41 -0500

    finish the demo

--- src/app/components/profile.component.ts
+++ src/app/components/profile.component.ts
@@ -12,10 +12,23 @@ import 'rxjs/add/operator/map';
 export class ProfileComponent  {
   private user: any;
   private repos:any;
+  private username:string;
   // 1. if you want to use a service or imported module, then inject them into the constructor
   // 2. add private cuz if you want to use the param everywhere in the component, then use private, otherwise you will only be able to use it inside the constructor
   constructor(private _githubService: GithubService){
+    this.user = false;
+    // this._githubService.getUser().subscribe(user => {
+    //   console.log(user.name);
+    //   this.user = user;
+    //   console.log(this.user.name);
+    // })
 
+    // this._githubService.getRepos().subscribe(repos => {
+    //   this.repos = repos;
+    // })
+  }
+  searchUser(){
+    this._githubService.updateUser(this.username);
     this._githubService.getUser().subscribe(user => {
       console.log(user.name);
       this.user = user;
--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -5,19 +5,23 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class GithubService{
     private username: string;
-
+    private client_id = '040c0d40d3fddfb47e99';
+    private client_secert = '8071535bc02bb468ff9cf4ddf577f21180bcf8cc';
     constructor(private _http:Http ){
         console.log("github service ready");
         this.username = 'bradtraversy';
     }
     getUser(){
         // return an observable therefore you need map to change the res to json formatted
-        return this._http.get('http://api.github.com/users/'+this.username)
+        return this._http.get('http://api.github.com/users/'+this.username+"?client_id="+this.client_id +"&client_secert"+this.client_secert)
             .map(res => res.json());
     }
     getRepos(){
         // return an observable therefore you need map to change the res to json formatted
-        return this._http.get('http://api.github.com/users/'+this.username+"/repos")
+        return this._http.get('http://api.github.com/users/'+this.username+"/repos"+"?client_id="+this.client_id +"&client_secert"+this.client_secert)
             .map(res => res.json());
+    }
+    updateUser(username:string){
+        this.username = username;
     }
 }
